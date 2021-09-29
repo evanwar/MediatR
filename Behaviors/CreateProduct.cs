3ade4d6dfee303d9c638c1938695e3fca08904f3
Evan <evan.af@hotmail.com>
2021-09-28 19:49:56 -0500

    Behavior
    Behavior

---
+++ Behaviors/CreateProduct.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Behaviors
+{
+    public class CreateProduct : IRequest<int>
+    {
+        public string Name { get; set; }
+    }
+}
